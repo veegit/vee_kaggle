733ecb2c36c904628198c69894bc8575bf4a0201
Vee <narayanan.venkiteswaran@appdirect.com>
2013-10-29 23:36:15 -0700

    views

--- fb/views.sql
+++ fb/views.sql
@@ -1,12 +1,35 @@
-insert into mv_count_tags
-select g.id, g.name, count(*) as count  from tags g, train_tag_mapping m where g.id = m.tag_id group by g.id, g.name order by count(*) desc;
+TRUNCATE TABLE mv_count_tags;
+INSERT INTO mv_count_tags
+SELECT g.id, g.name, COUNT(*) AS COUNT
+FROM tags g, train_tag_mapping m
+WHERE g.id = m.tag_id
+GROUP BY g.id, g.name
+ORDER BY COUNT(*) DESC;
 
+SELECT 'DONE 1 DIM' as '';
 
-insert into mv_x2_count_tags
-SELECT m.tag_id as id1,m2.tag_id as id2,count(*) as count
-FROM tags g, train_tag_mapping m, train_tag_mapping m2
-WHERE g.id = m.tag_id 
-AND m2.train_id = m.train_id
-AND m2.tag_id <> m.tag_id
-group by m.tag_id,m2.tag_id
-order by count(*) desc
+TRUNCATE TABLE mv_x2_count_tags;
+INSERT INTO mv_x2_count_tags(id1,id2,count)
+SELECT m1.tag_id AS id1,m2.tag_id AS id2, COUNT(*) AS COUNT
+FROM tags g, train_tag_mapping m1, train_tag_mapping m2
+WHERE g.id = m1.tag_id 
+AND m1.train_id = m2.train_id 
+AND m1.tag_id <> m2.tag_id
+GROUP BY m1.tag_id,m2.tag_id
+ORDER BY COUNT(*) DESC;
+
+SELECT 'DONE 2 DIM' as '';
+
+TRUNCATE TABLE mv_x3_count_tags;
+INSERT INTO mv_x3_count_tags(id1,id2,id3,count)
+SELECT m1.tag_id AS id1, m2.tag_id AS id2, m3.tag_id AS id3, COUNT(*) AS COUNT
+FROM tags g, train_tag_mapping m1, train_tag_mapping m2, train_tag_mapping m3
+WHERE g.id = m1.tag_id 
+AND m1.train_id = m2.train_id 
+AND m2.train_id = m3.train_id 
+AND m1.tag_id <> m2.tag_id 
+AND m2.tag_id <> m3.tag_id
+GROUP BY m1.tag_id, m2.tag_id, m3.tag_id
+ORDER BY COUNT(*) DESC;
+
+SELECT 'DONE 3 DIM' as '';
